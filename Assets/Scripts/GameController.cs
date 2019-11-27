8f399a3834e230b500b36c2d5e300cf7410dc378
Divyaswor Makai <makaidivya@gmail.com>
2019-11-27 21:36:37 +0545

    Initial UI ad changes

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    void Start()
+    {
+        int mode = PlayerPrefs.GetInt("Mode");
+        print(mode);
+
+        int counter = 0;
+        int [,]matrix = new int[mode,mode];
+
+        List<int> done = new List<int>();
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                bool complete = true;
+                while (complete)
+                {
+                    int rand = Random.Range(0, mode * mode);
+                    if (done.Contains(rand))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        matrix[i, j] = rand;
+                        done.Add(rand);
+                        complete = false;
+                    }
+                }
+            }
+        }
+        print(mode);
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                print(matrix[i,j]);
+            }
+        }
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Menu");
+        } 
+    }
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void EasyMode()
+    {
+        PlayerPrefs.SetInt("Mode", 3);
+        SceneManager.LoadScene("MainGame");
+    }
+    public void MediumMode()
+    {
+        PlayerPrefs.SetInt("Mode", 4);
+        SceneManager.LoadScene("MainGame");
+    }
+    public void HardMode()
+    {
+        PlayerPrefs.SetInt("Mode", 5);
+        SceneManager.LoadScene("MainGame");
+    }
+}
